5183ea6dc64ae8cc8846e47b89119b95e4159f4c
Pedro García <pgnovato@gmail.com>
2022-04-25 18:59:00 -0500

    Primer Commit

---
+++ dist/models/server.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+class Server {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.port = process.env.PORT || '2000';
+        //  Middlewares
+        this.middlewares();
+    }
+    middlewares() {
+        this.app.use((0, cors_1.default)());
+        this.app.use(express_1.default.json());
+    }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en el puerto ${this.port}`);
+        });
+    }
+}
+exports.default = Server;
+//# sourceMappingURL=server.js.map
